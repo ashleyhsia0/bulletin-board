89d08ecfb64d3e53de8d02aecf864652fcb74ee2
Ashley Hsiao <aiyihsiao@gmail.com>
2016-10-16 01:21:24 -0700

    Import and render Board

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Board from './Board';
 import './index.css';
 
 ReactDOM.render(
-  <App />,
+  <Board count={10} />,
   document.getElementById('root')
 );
